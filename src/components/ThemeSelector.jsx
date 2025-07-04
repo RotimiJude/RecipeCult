eb7051c0e32c4e721d62ef9602aa24fba339ed7d
Rotimi Jude <excel.rotimi@gmail.com>
2025-07-04 21:30:23 +0100

    message

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -11,10 +11,8 @@ export default function ThemeSelector() {
   return (
     <div className="flex justify-evenly items-center space-x-7 py-3">
         <div>
-        <svg onClick={toggleMode} style={{filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)'}} className="size-10 text-gray-400 peer-focus:text-gray-600 duration-500 cursor-pointer" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-width="2" d="M7 17v1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-4a3 3 0 0 0-3 3Zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
-                      </svg>
-        </div>
+         <svg xmlns="http://www.w3.org/2000/svg" onClick={toggleMode} style={{filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)'}} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-sun-icon lucide-sun size-10 text-gray-400 peer-focus:text-gray-600 duration-500 cursor-pointer"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>
+        </div>       
         {themeColors.map(color => (
             <div key={color}
             onClick={() => changeColor(color)}
